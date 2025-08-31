7c19e24f6059338af9b4f457516e48b811585f34
lawrence-ruscia <140231442+lawrence-ruscia@users.noreply.github.com>
2025-08-31 14:05:51 +0800

    feat: create details page for each message item

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,18 +1,21 @@
 const messages = [
   {
+    id: '1',
     message: 'Hi there!',
     username: 'Amando',
-    added: new Date(),
+    added: new Date('July 25, 2025 02:26:00'),
   },
   {
+    id: '2',
     message: 'Hello World!',
     username: 'Charles',
-    added: new Date(),
+    added: new Date('August 13, 2025 11:13:00'),
   },
   {
+    id: '3',
     message: 'Wanker!',
     username: 'John Constantine',
-    added: new Date(),
+    added: new Date('August 20, 2025 04:09:00'),
   },
 ];
 
@@ -40,16 +43,37 @@ function getInitials(name) {
 }
 
 export const getDashboard = (req, res) => {
-  res.render('index', { messages, formatDate, getInitials });
+  res.render('index', {
+    messages,
+    formatDate,
+    getInitials,
+  });
 };
 
 export const getMessageForm = (req, res) => {
   res.render('messageForm', { messages });
 };
 
+export const getMessageDetails = (req, res) => {
+  const { id } = req.params ?? '';
+  const retrievedMessage = messages.find((msg) => msg.id === id);
+
+  res.render('messageDetails', {
+    messages,
+    msg: retrievedMessage,
+    formatDate,
+    getInitials,
+  });
+};
+
 export const createMessagePost = (req, res) => {
   const { username, message } = req.body;
-  messages.push({ username, message, added: new Date(Date.now()) });
+  messages.push({
+    id: crypto.randomUUID(),
+    username,
+    message,
+    added: new Date(Date.now()),
+  });
 
   res.redirect('/');
 };
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router, urlencoded } from 'express';
 import {
   getDashboard,
   getMessageForm,
+  getMessageDetails,
   createMessagePost,
 } from '../controllers/indexController.js';
 
@@ -9,5 +10,6 @@ export const index = Router();
 
 index.get('/', getDashboard);
 index.get('/new', getMessageForm);
+index.get('/:id', getMessageDetails);
 
 index.post('/new', createMessagePost);
